10006d8ce31c5040228a4db7d93b390095cdb5a4
ralexandre75 <rene.alexandre@grimoire-design.fr>
2018-04-20 17:05:19 +0200

    card

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule,
+		 MatCardModule } from '@angular/material';
 
 const MATERIAL = [
-	MatButtonModule
+	MatButtonModule,
+	MatCardModule
 ]
 
 @NgModule({
